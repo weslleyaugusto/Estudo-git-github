0b121ef3f473a8b3a9662a8d6da34215c87ea010
weslleyaugusto <137814164+weslleyaugusto@users.noreply.github.com>
2026-06-05 20:02:32 -0300

    Adição teste merge sout2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Branch teste");
         System.out.println("Teste de merge conflitante");
+        System.out.println("teste de merge 2");
     }
 }
