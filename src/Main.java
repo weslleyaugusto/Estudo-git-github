f97db7ef32244e9414d614590dc8a0b07bd55a66
weslleyaugusto <137814164+weslleyaugusto@users.noreply.github.com>
2026-06-05 19:48:51 -0300

    Adição de System.out

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         int age;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Branch teste");
-
+        System.out.println("Teste de merge conflitante");
     }
 }
